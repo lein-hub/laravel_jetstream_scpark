09588e4254a3115f3f5fc9b0fe89881eeb2e00ea
lein-hub <75424893+lein-hub@users.noreply.github.com>
2021-09-15 15:16:04 +0900

    comment update

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -13,79 +13,84 @@ use Intervention\Image\ImageManagerStatic;
 
 class Comments extends Component
 {
-    public $newComment;
-    public $image;
-    use WithPagination;
-    use WithFileUploads;
+  public $newComment;
+  public $image;
+  use WithPagination;
+  use WithFileUploads;
 
-    protected $listeners = [
-        'delete' => 'remove'
-    ];
+  protected $listeners = [
+    'delete' => 'remove',
+    'commentUpdated' => 'getComments'
+  ];
 
-    protected $rules = [
-        'newComment' => 'required',
-        'image' => 'nullable|image|max:10240',
-    ];
+  protected $rules = [
+    'newComment' => 'required',
+    'image' => 'nullable|image|max:10240',
+  ];
 
-    public function updated($propertyName)
-    {
-        $this->validateOnly($propertyName);
+  public function updated($propertyName)
+  {
+    $this->validateOnly($propertyName);
+  }
+
+  public function remove($commentId)
+  {
+    $comment = Comment::find($commentId);
+    if ($this->image) {
+      Storage::disk('public')->delete('images/' . $comment->image);
     }
+    $comment->delete();
 
-    public function remove($commentId)
-    {
-        $comment = Comment::find($commentId);
-        if ($this->image) {
-            Storage::disk('public')->delete('images/' . $comment->image);
-        }
-        $comment->delete();
+    session()->flash("message", "댓글이 성공적으로 삭제 되었습니다.");
+  }
 
-        session()->flash("message", "댓글이 성공적으로 삭제 되었습니다.");
-    }
+  public function mount()
+  {
+    // $this->newComment = "default world";
+  }
 
-    public function mount()
-    {
-        // $this->newComment = "default world";
-    }
+  public function render()
+  {
+    return view('livewire.comments', [
+      'comments' => Comment::latest()->paginate(5),
+    ]);
+  }
 
-    public function render()
-    {
-        return view('livewire.comments', [
-            'comments' => Comment::latest()->paginate(5),
-        ]);
-    }
+  public function getComments()
+  {
+  }
 
-    public function addComment()
-    {
-        $this->validate();
-        // $comment = new Comment();
-        // $comment->user_id = Auth::user()->id;
-        // $comment->save();
+  public function addComment()
+  {
+    $this->validate();
+    // $comment = new Comment();
+    // $comment->user_id = Auth::user()->id;
+    // $comment->save();
 
-        // 이미지가 있으면 원하는 폴더에 저장하고 저장된 파일의 이름을 기억한다. $imageFileName
-        $imageFileName = null;
-        if ($this->image) $imageFileName = $this->storeImage();
+    // 이미지가 있으면 원하는 폴더에 저장하고 저장된 파일의 이름을 기억한다. $imageFileName
+    $imageFileName = null;
+    if ($this->image) $imageFileName = $this->storeImage();
 
-        Comment::create(
-            [
-                'user_id' => auth()->user()->id,
-                'content' => $this->newComment,
-                'image' => $imageFileName,
-            ]
-        );
+    Comment::create(
+      [
+        'user_id' => auth()->user()->id,
+        'content' => $this->newComment,
+        'image' => $imageFileName,
+      ]
+    );
 
-        $this->newComment = '';
-        $this->image = '';
-        session()->flash("message", "댓글이 성공적으로 등록 되었습니다.");
-    }
+    $this->newComment = '';
+    $this->image = '';
+    session()->flash("message", "댓글이 성공적으로 등록 되었습니다.");
+  }
 
-    protected function storeImage()
-    {
-        $img = ImageManagerStatic::make($this->image)->resize(200, 100)->encode('jpg');
-        $name = Str::random() . '.png';
-        // $this->image->storeAs('public/images', $name);
-        Storage::disk('public')->put('images/' . $name, $img);
+  protected function storeImage()
+  {
+    $img = ImageManagerStatic::make($this->image)->resize(200, 100)->encode('jpg');
+    $name = Str::random() . '.png';
+    // $this->image->storeAs('public/images', $name);
+    Storage::disk('public')->put('images/' . $name, $img);
 
-        return $name;
-    }
+    return $name;
+  }
 }
--- app/Http/Livewire/EditComment.php
+++ app/Http/Livewire/EditComment.php
@@ -3,6 +3,9 @@
 namespace App\Http\Livewire;
 
 use App\Models\Comment;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Intervention\Image\ImageManagerStatic;
 use Livewire\WithFileUploads;
 use LivewireUI\Modal\ModalComponent;
 
@@ -19,9 +22,47 @@ class EditComment extends ModalComponent
     'update' => 'updateComment'
   ];
 
+  protected $rules = [
+    'newComment' => 'required',
+    'image' => 'nullable|image|max:10240',
+  ];
+
   public function updateComment()
   {
+    $this->validate();
+    // $comment = new Comment();
+    // $comment->user_id = Auth::user()->id;
+    // $comment->save();
+
+    // 이미지가 있으면 원하는 폴더에 저장하고 저장된 파일의 이름을 기억한다. $imageFileName
+    $imageFileName = null;
+    if ($this->image) {
+      // 기존 Comment 객체의 이미지가 있는지 확인하고
+      // 있다면 파일시스템에서 삭제를 먼저 한다.
+
+      if ($this->orgComment->image) {
+        // 삭제하자
+        Storage::disk('public')->delete('images/' . $this->orgComment->image);
+      }
+      $imageFileName = $this->storeImage();
+      $this->orgComment->image = $imageFileName;
+    }
+    $this->orgComment->content = $this->newComment;
+    $this->orgComment->save();
+
+    session()->flash("message", "댓글이 성공적으로 업데이트 되었습니다.");
+    $this->emit('commentUpdated');
     $this->closeModal();
+  }
+
+  protected function storeImage()
+  {
+    $img = ImageManagerStatic::make($this->image)->resize(200, 200)->encode('jpg');
+    $name = Str::random() . '.png';
+    // $this->image->storeAs('public/images', $name);
+    Storage::disk('public')->put('images/' . $name, $img);
+
+    return $name;
   }
 
   public function render()
